01161a190f3bfcb56f712aec337864d3d96c65ad
kaleemiqbal4 <olduser0071@gmail.com>
2020-12-22 01:50:05 +0530

    Worked on helper class

---
+++ login/login_business.py
@@ -0,0 +1,6 @@
+import sys
+
+class loginBusiness:
+    def userInfo(self):
+        print("hello " + self)
+        return self + " failed name"
--- login/views.py
+++ login/views.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from login.models import User
 from login.serializers import UserSerializer
 from rest_framework.decorators import api_view
-
+from login.login_business import loginBusiness
 
 @api_view(['POST'])
 def user_list(request):
@@ -16,6 +16,9 @@ def user_list(request):
         user_data = JSONParser().parse(request)
         user_serializer = UserSerializer(data=user_data)
         if user_serializer.is_valid():
+            name = loginBusiness.userInfo(user_data.get('first_name'))
+            user_serializer.first_name = name
+            print('hello called second ' + name)
             user_serializer.save()
             return JsonResponse(user_serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
